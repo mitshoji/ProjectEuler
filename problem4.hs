7bdcfa685a0a730c82b4ad90452be0e24d0b8ce3
mitshoji <mitshackish@gmail.com>
2020-03-30 21:49:07 +0900

    first upload

---
+++ problem4.hs
@@ -0,0 +1,22 @@
+{-
+Problem 4
+   A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+
+   Find the largest palindrome made from the product of two 3-digit numbers.
+-}
+import Data.List
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome x = x == reverse x
+
+
+candidates :: [Int]
+candidates = (reverse . sort) [ x * y | x <- [101..999], y <- [x..999]]
+-- using head with (reverse.sort) is faster than using last with sort
+
+
+main :: IO ()
+main = print $ head $ filter isPalindrome $ map show candidates
+
+
+--  Answer: 906609
