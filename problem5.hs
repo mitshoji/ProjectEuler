75da17fd6375f683164af40e9b784011cf2a6519
mitshoji <mitshackish@gmail.com>
2020-04-01 22:40:19 +0900

    needless to coding

---
+++ problem5.hs
@@ -0,0 +1,16 @@
+{-
+Smallest multiple
+   
+Problem 5
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+-}
+
+--The problem can be solved by "mind calculation"
+
+main :: IO ()
+main = print $ 2^4 * 3^2 * 5 * 7 * 11 * 13 * 17 * 19
+
+
+--Answer: 232792560
