030c6f172cbaaf7527ec77fa0a222850a6535e62
mitshoji <mitshackish@gmail.com>
2020-04-05 06:32:14 +0900

    fast prime generator

--- problem10.hs
+++ problem10.hs
@@ -7,20 +7,15 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 -}
 
-newPrimes :: [Integer]
-newPrimes = 2 : filter isPrime [3,5..]
+primes :: [Integer]
+primes = 2 : filter isPrime [3,5..]
 
 isPrime :: Integer -> Bool
-isPrime n = all (\d -> n `mod`d /= 0) [ x | x <- [2..bound n]]
-    where
-       bound q = sqrint [1..q] q
-       sqrint (r:rs) q | (r * r) <  q = sqrint rs q
-                       | (r * r) == q = r
-                       | (r * r) >  q = r-1
+isPrime n = all (\d -> n `mod`d /= 0) [ x | x <- takeWhile (\d -> d*d <= n) primes]
 
 
 main :: IO ()
-main = print $ sum $ takeWhile ( < 2000000) newPrimes
+main = print $ sum $ takeWhile ( < 2000000) primes
 
 
 
