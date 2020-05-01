7f95840dab8c4b061dbad8aedec4d957c98d31f5
mitshoji <mitshackish@gmail.com>
2020-05-01 09:02:17 +0900

    truncateL&R

--- problem21-40/problem37.hs
+++ problem21-40/problem37.hs
@@ -6,18 +6,18 @@ main = print $ sum $ take 11 $ filter isTruncatable $ drop 4 primes
 -- drop 4 is to pick out first 4 primes; 2,3,5,7
 
 isTruncatable :: Int -> Bool
-isTruncatable n = all isPrime $ truncate n
+isTruncatable n = all isPrime $ truncateLR n
 
-truncate :: Int -> [Int]
-truncate n = (truncateL n) ++ (tail $ truncateR n) 
+truncateLR :: Int -> [Int]
+truncateLR n = (tail $ truncateL n) ++ (truncateR n) 
 
-truncateR :: Int -> [Int]
-truncateR = (map read). init. tails. show
--- truncateR 247 = [247,47,7]
+truncateL :: Int -> [Int]
+truncateL = (map read). init. tails. show
+-- truncateL 247 = [247,47,7]
 -- we need init since tails returns empty element at last,
 -- like [247,47,7,""]
 
-truncateL :: Int -> [Int]
-truncateL = (map read). tail. inits. show
--- truncateL 247 = [2,24,247]
+truncateR :: Int -> [Int]
+truncateR = (map read). tail. inits. show
+-- truncateR 247 = [2,24,247]
 -- In truncateL, we need tail as we do in truncateR for the same reason.
